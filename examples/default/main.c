0f024a0effbf9b94ed510795597b4dfe3a53c90b
zhuoshuguo <shuguo.zhuo@inria.fr>
2016-10-05 10:29:48 +0200

    Add sender codes.

--- examples/default/main.c
+++ examples/default/main.c
@@ -28,10 +28,17 @@
 #include "thread.h"
 #include "shell.h"
 #include "shell_commands.h"
+#include "timex.h"
+#include "net/gnrc.h"
+#include "net/gnrc/netif.h"
+#include "net/gnrc/netapi.h"
+#include "xtimer.h"
 
+/*
 #if FEATURE_PERIPH_RTC
 #include "periph/rtc.h"
 #endif
+*/
 
 #ifdef MODULE_LTC4150
 #include "ltc4150.h"
@@ -42,15 +49,98 @@
 #include "net/gnrc.h"
 #endif
 
+uint32_t send_counter;
+
+static void generate_and_send_pkt(void){
+
+	   kernel_pid_t dev;
+	    uint8_t addr[8];
+	    size_t addr_len;
+	    gnrc_pktsnip_t *hdr;
+	    int16_t dev2;
+
+	    gnrc_pktsnip_t* pkt;
+	    uint32_t payload[12];
+
+	    send_counter++;
+
+	    payload[0] = send_counter;
+
+	    dev2 = 4;
+	    /* parse interface */
+	    dev = (kernel_pid_t)dev2;
+
+	    addr_len = 8;
+	    addr[0] = 0x5a;
+	    addr[1] = 0x55;
+	    addr[2] = 0x60;
+	    addr[3] = 0x5c;
+	    addr[4] = 0x51;
+	    addr[5] = 0x09;
+	    addr[6] = 0x44;
+	    addr[7] = 0x7e;
+
+	    hdr = gnrc_netif_hdr_build(NULL, 0, addr, addr_len);
+	    if(hdr == NULL){
+	    	puts("app: netif buf null!");
+	    	send_counter --;
+	    	return;
+	    }
+
+		/****** assemble and send the beacon ******/
+		pkt = gnrc_pktbuf_add(NULL, payload, sizeof(payload), GNRC_NETTYPE_UNDEF);
+		if(pkt == NULL) {
+
+			gnrc_pktbuf_release(hdr);
+
+			send_counter --;
+			puts("app: data buf null!");
+		}else{
+		    LL_PREPEND(pkt, hdr);
+
+		    gnrc_netapi_send(dev, pkt);
+
+		    printf("p: %lu.\n", send_counter);
+		}
+
+}
+
+
+void *sender_thread(void *arg)
+{
+    (void) arg;
+
+    //printf("shuguo-app thread started, pid: %" PRIkernel_pid "\n", thread_getpid());
+
+    send_counter = 0;
+
+    xtimer_sleep(10);
+
+    while (1) {
+
+    	xtimer_sleep(3);
+    	//xtimer_usleep(500000);
+
+    	if(send_counter <5000){
+    		for(int i=0; i<1; i++){
+    			generate_and_send_pkt();
+    		}
+    	}
+
+    }
+
+    return NULL;
+}
+
+char second_thread_stack[THREAD_STACKSIZE_MAIN];
+
+
 int main(void)
 {
 #ifdef MODULE_LTC4150
     ltc4150_start();
 #endif
 
-#ifdef FEATURE_PERIPH_RTC
-    rtc_init();
-#endif
 
 #ifdef MODULE_NETIF
     gnrc_netreg_entry_t dump;
@@ -59,6 +149,12 @@ int main(void)
     dump.demux_ctx = GNRC_NETREG_DEMUX_CTX_ALL;
     gnrc_netreg_register(GNRC_NETTYPE_UNDEF, &dump);
 #endif
+
+//kernel_pid_t pid =
+    thread_create(second_thread_stack, sizeof(second_thread_stack),
+                            THREAD_PRIORITY_MAIN + 1, THREAD_CREATE_STACKTEST,
+                            sender_thread, NULL, "shuguo_app");
+
 
     (void) puts("Welcome to RIOT!");
 
