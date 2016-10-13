ba9cb412db44e5ee7c18d7f216e8b5d77f37ffc4
zhuoshuguo <shuguo.zhuo@inria.fr>
2016-10-13 18:44:28 +0200

    Add basic gnrc_mac type file.

---
+++ sys/include/net/gnrc/gnrc_mac/gnrc_mac_types.h
@@ -0,0 +1,67 @@
+/*
+ * Copyright (C) 2015 Daniel Krebs
+ *
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    net_gnrc_mac  A common MAC type for providing key MAC parameters and helper functions
+ * @ingroup     net
+ * @brief       A common MAC type for providing key MAC parameters and helper functions.
+ * @{
+ *
+ * @file
+ * @brief       Internal types used by the GNRC_MAC entities
+ *
+ * @author      Daniel Krebs <github@daniel-krebs.net>
+ * @author      Shuguo Zhuo  <shuguo.zhuo@inria.fr>
+ */
+
+#ifndef GNRC_MAC_TYPES_H_
+#define GNRC_MAC_TYPES_H_
+
+#include <stdint.h>
+#include <stdbool.h>
+#include <kernel_types.h>
+#include <xtimer.h>
+#include <net/netdev2.h>
+#include <net/gnrc/netdev2.h>
+#include <net/gnrc.h>
+
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/******************************************************************************/
+
+typedef enum {
+    TX_FEEDBACK_UNDEF = -1,
+    TX_FEEDBACK_SUCCESS,
+    TX_FEEDBACK_NOACK,
+    TX_FEEDBACK_BUSY
+} gnrc_mac_tx_feedback_t;
+
+/******************************************************************************/
+typedef struct gnrc_mac {
+
+	/* NETDEV device used by MAC */
+	gnrc_netdev2_t* gnrc_netdev2;
+    
+    /* Track if a transmission might have corrupted a received packet */
+    bool rx_started;
+
+    /* Feedback of last packet that was sent */
+    gnrc_mac_tx_feedback_t tx_feedback;
+
+} gnrc_mac_t;
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GNRC_MAC_TYPES_H_ */
+/** @} */
