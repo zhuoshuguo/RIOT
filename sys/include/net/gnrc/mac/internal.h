ac5d118eff57f94365e11ea3e66f149774c5fc58
zhuoshuguo <shuguo.zhuo@inria.fr>
2016-11-07 18:18:19 +0100

    gnrc_mac: add mac internal helper functions.

---
+++ sys/include/net/gnrc/mac/internal.h
@@ -0,0 +1,83 @@
+/*
+ * Copyright (C) 2015 Daniel Krebs
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @defgroup    net_gnrc_mac  A common MAC type for providing key MAC parameters and helper functions
+ * @ingroup     net
+ * @brief       A common MAC type for providing key MAC parameters and helper functions.
+ * @{
+ *
+ * @file
+ * @brief       Interface definition for internal functions of MAC protocol
+ *
+ * @author      Daniel Krebs <github@daniel-krebs.net>
+ * @author      Shuguo Zhuo  <shuguo.zhuo@inria.fr>
+ */
+
+#ifndef GNRC_MAC_INTERNAL_H_
+#define GNRC_MAC_INTERNAL_H_
+
+#include <stdint.h>
+#include <types.h>
+#include <net/ieee802154.h>
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* @brief Extract the destination address out of an GNRC_NETTYPE_NETIF pktsnip
+ *
+ * @param[in]   pkt                 pktsnip from whom to extract
+ * @param[out]  pointer_to_addr     pointer to address will be stored here
+ *
+ * @return                          length of destination address
+ */
+int _get_dest_address(gnrc_pktsnip_t* pkt, uint8_t* pointer_to_addr[]);
+
+/* @brief Find the first pktsnip of @p type
+ *
+ * Will search linearly through the packet buffer @p pkt and yield
+ * gnrc_pktsnip_t::data of the first pktsnip match the type @p type.
+ *
+ * @param[in]   pkt     pktsnip that will be searched
+ * @param[in]   type    type to search for
+ *
+ * @return              pointer to data, NULL is not found
+ */
+void* _gnrc_pktbuf_find(gnrc_pktsnip_t* pkt, gnrc_nettype_t type);
+
+/* @brief Check if packet is broadcast
+ *
+ * @param[in]   pkt             packet to check
+ */
+static inline bool _packet_is_broadcast(gnrc_pktsnip_t* pkt)
+{
+    gnrc_netif_hdr_t* netif_hdr = _gnrc_pktbuf_find(pkt, GNRC_NETTYPE_NETIF);
+    return ( (netif_hdr == NULL) ? false :
+                              (netif_hdr->flags & (GNRC_NETIF_HDR_FLAGS_BROADCAST | GNRC_NETIF_HDR_FLAGS_MULTICAST)) );
+}
+
+
+static inline bool _addr_match( uint8_t * addr1, uint8_t* addr2, uint8_t add_len)
+{
+    assert(addr1);
+    assert(addr2);
+    assert(add_len);
+
+    if(add_len > IEEE802154_LONG_ADDRESS_LEN)
+        return false;
+
+    return (memcmp(addr1, addr2, add_len) == 0);
+}
+
+#ifdef __cplusplus
+}
+#endif
+
+#endif /* GNRC_MAC_INTERNAL_H_ */
+/** @} */
---
+++ sys/net/gnrc/link_layer/gnrc_mac/internal.c
@@ -0,0 +1,65 @@
+/*
+ * Copyright (C) 2015 Daniel Krebs
+ *
+ * This file is subject to the terms and conditions of the GNU Lesser
+ * General Public License v2.1. See the file LICENSE in the top level
+ * directory for more details.
+ */
+
+/**
+ * @{
+ * @ingroup     net_gnrc_mac
+ * @file
+ * @brief       Internal functions of GNRC_WMAC
+ *
+ * @author      Daniel Krebs <github@daniel-krebs.net>
+ * @author      Shuguo Zhuo  <shuguo.zhuo@inria.fr>
+ * @}
+ */
+
+#include <stdbool.h>
+#include <net/gnrc.h>
+#include <net/gnrc/mac/internal.h>
+
+#define ENABLE_DEBUG    (0)
+#include "debug.h"
+
+/******************************************************************************/
+
+int _get_dest_address(gnrc_pktsnip_t* pkt, uint8_t* pointer_to_addr[])
+{
+    int res;
+    gnrc_netif_hdr_t* netif_hdr;
+
+    if(!pkt)
+        return -ENODEV;
+
+    gnrc_pktsnip_t* netif_snip = _gnrc_pktbuf_find(pkt, GNRC_NETTYPE_NETIF);
+
+    if(netif_snip) {
+        netif_hdr = (gnrc_netif_hdr_t*) netif_snip->data;
+    } else {
+        return -ENOENT;
+    }
+
+    if((res = netif_hdr->dst_l2addr_len) <= 0)
+        return -ENOENT;
+
+    *pointer_to_addr = gnrc_netif_hdr_get_dst_addr(netif_hdr);
+    return res;
+}
+
+/******************************************************************************/
+
+/* Find a payload based on it's protocol type */
+void* _gnrc_pktbuf_find(gnrc_pktsnip_t* pkt, gnrc_nettype_t type)
+{
+    while(pkt != NULL)
+    {
+        if(pkt->type == type) {
+            return pkt->data;
+        }
+        pkt = pkt->next;
+    }
+    return NULL;
+}
