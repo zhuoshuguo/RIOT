1ae6aa400866499950767f4d33a59118dc04a6ae
zhuoshuguo <shuguo.zhuo@inria.fr>
2017-02-24 22:00:37 +0100

    Fixed lwmac.h's comment style.

--- sys/include/net/gnrc/lwmac/lwmac.h
+++ sys/include/net/gnrc/lwmac/lwmac.h
@@ -34,116 +34,142 @@ typedef struct gnrc_netdev2 gnrc_netdev2_t;
 extern "C" {
 #endif
 
-/* Time between consecutive wakeups. This parameter governs power consumption,
- * latency and throughput! */
+/**
+ * @brief Time between consecutive wakeups. This parameter governs power consumption,
+ *        latency and throughput!
+ */
 #ifndef LWMAC_WAKEUP_INTERVAL_US
 #define LWMAC_WAKEUP_INTERVAL_US        (100U * 1000)
 #endif
 
-/* The Maximum WR duration time */
+/**
+ * @brief The Maximum WR duration time.
+ */
 #ifndef LWMAC_PREAMBLE_DURATION_US
 #define LWMAC_PREAMBLE_DURATION_US      ((12*LWMAC_WAKEUP_INTERVAL_US)/10)
 #endif
 
+/**
+ * @brief The Maximum random backoff time before sending WR.
+ */
 #ifndef LWMAC_RANDOM_BEFORE_WR_US
 #define LWMAC_RANDOM_BEFORE_WR_US        (1000U)
 #endif
 
-/* Timeout to send the next WR in case no WA has been received during that
- * time. It is referenced to the beginning of both WRs, but due to internal
- * overhead, the exact spacing is slightly higher.
- * The minimum possible value depends on the time it takes to completely
- * send a WR with the given hardware (including processor) and data rate. */
+/**
+ * @brief Timeout to send the next WR in case no WA has been received during that
+ *        time. It is referenced to the beginning of both WRs, but due to internal
+ *        overhead, the exact spacing is slightly higher.
+ *        The minimum possible value depends on the time it takes to completely
+ *        send a WR with the given hardware (including processor) and data rate.
+ */
 #ifndef LWMAC_TIME_BETWEEN_WR_US
 #define LWMAC_TIME_BETWEEN_WR_US        (7000U)
 #endif
 
-/* Time to idle between two successive BROADCAST packets, referenced to the
- * start of the packet. The same limitation as for time-between-WR apply here. */
+/**
+ * @brief Time to idle between two successive BROADCAST packets, referenced to the
+ *        start of the packet. The same limitation as for time-between-WR apply here.
+ */
 #ifndef LWMAC_TIME_BETWEEN_BROADCAST_US
 #define LWMAC_TIME_BETWEEN_BROADCAST_US (LWMAC_TIME_BETWEEN_WR_US)
 #endif
 
-/* Receiver needs to support RX_START event in order to use time-between-WR
- * as a sensible default here. Otherwise the duration of WRs as well as longest
- * possible data broadcasts need to be taken into account. */
+/**
+ * @brief Receiver needs to support RX_START event in order to use time-between-WR
+ *        as a sensible default here. Otherwise the duration of WRs as well as longest
+ *        possible data broadcasts need to be taken into account.
+ */
 #ifndef LWMAC_WAKEUP_DURATION_US
 #define LWMAC_WAKEUP_DURATION_US        (LWMAC_TIME_BETWEEN_WR_US * 2)
 #endif
 
-/* Start sending earlier then known phase. Therefore advance to beginning edge
- * of destinations wakeup phase over time.
- * Note: * RTT tick is ~30us @ 32 kHz timer
- *       * there is a certain overhead from dispatching driver call until WR
- *         will be really sent that may depend on hardware and driver
- *         implementation
+/**
+ * @brief Start sending earlier then known phase. Therefore advance to beginning edge
+ *        of destinations wakeup phase over time.
+ * Note:  RTT tick is ~30us @ 32 kHz timer.
+ *        There is a certain overhead from dispatching driver call until WR
+ *        will be really sent that may depend on hardware and driver
+ *        implementation.
  */
 #ifndef LWMAC_WR_BEFORE_PHASE_US
 #define LWMAC_WR_BEFORE_PHASE_US        (1300U)
 #endif
 
-/* WR preparation overhead before it can be sent (higher with debugging output).
- * LwMAC will wakeup earlier to prepare for synced WR sending. When preparation
- * is done, it will busy wait (block the whole system) until the WR has been
- * dispatched to the driver. */
+/**
+ * @brief WR preparation overhead before it can be sent (higher with debugging output).
+ *        LwMAC will wakeup earlier to prepare for synced WR sending. When preparation
+ *        is done, it will busy wait (block the whole system) until the WR has been
+ *        dispatched to the driver.
+ */
 #ifndef LWMAC_WR_PREPARATION_US
 #define LWMAC_WR_PREPARATION_US         (7000U + LWMAC_WR_BEFORE_PHASE_US)
 #endif
 
-/* How long to wait after a WA for data to come in. It's enough to catch the
- * beginning of the packet if the transceiver supports RX_STARTED event (this
- * can be important for big packets). */
+/**
+ * @brief How long to wait after a WA for data to come in. It's enough to catch the
+ *        beginning of the packet if the transceiver supports RX_STARTED event (this
+ *        can be important for big packets).
+ */
 #ifndef LWMAC_DATA_DELAY_US
 #define LWMAC_DATA_DELAY_US             (5000U)
 #endif
 
-/* How long BROADCAST packets will be sent to make sure every participant has
- * received at least one packet. */
+/**
+ * @brief How long BROADCAST packets will be sent to make sure every participant has
+ *        received at least one packet.
+ */
 #ifndef LWMAC_BROADCAST_DURATION_US
 #define LWMAC_BROADCAST_DURATION_US     ((LWMAC_WAKEUP_INTERVAL_US * 1100) / 1000)
 #endif
 
-/* Max link layer address length in bytes */
+/**
+ * @brief Max link layer address length in bytes.
+ */
 #ifndef LWMAC_MAX_L2_ADDR_LEN
 #define LWMAC_MAX_L2_ADDR_LEN           (8U)
 #endif
 
-/* CSMA retries for DATA packet after WR->WA was successful. Too many retries
- * may timeout the receiver, refer LWMAC_DATA_DELAY_US */
+/**
+ * @brief CSMA retries for DATA packet after WR->WA was successful. Too many retries
+ *         may timeout the receiver, refer LWMAC_DATA_DELAY_US.
+ */
 #ifndef LWMAC_DATA_CSMA_RETRIES
 #define LWMAC_DATA_CSMA_RETRIES         (3U)
 #endif
 
-/* CSMA retries for BROADCAST packet, too many may lead to running out of
- * destinations wakup period. */
+/**
+ * @brief CSMA retries for BROADCAST packet, too many may lead to running out of
+ *        destinations wakup period.
+ */
 #ifndef LWMAC_BROADCAST_CSMA_RETRIES
 #define LWMAC_BROADCAST_CSMA_RETRIES    (3U)
 #endif
 
 /**
- * @brief   Set the default message queue size for LWMAC layer
+ * @brief Set the default message queue size for LWMAC layer
  */
 #ifndef LWMAC_IPC_MSG_QUEUE_SIZE
 #define LWMAC_IPC_MSG_QUEUE_SIZE        (8U)
 #endif
 
 /**
- * @brief   Count of parallel timeouts. Shouldn't needed to be changed.
+ * @brief Count of parallel timeouts. Shouldn't needed to be changed.
  */
 #ifndef LWMAC_TIMEOUT_COUNT
 #define LWMAC_TIMEOUT_COUNT             (3U)
 #endif
 
 /**
- * @brief   Enable/disable duty-cycle record and print out.
- *          Set "1" to enable, set "0" to disable.
+ * @brief Enable/disable duty-cycle record and print out.
+ *        Set "1" to enable, set "0" to disable.
  */
 #ifndef LWMAC_ENABLE_DUTYCYLE_RECORD
 #define LWMAC_ENABLE_DUTYCYLE_RECORD             (1U)
 #endif
 
 /**
- * @brief   Initialize an instance of the LWMAC layer
+ * @brief Initialize an instance of the LWMAC layer
  *
  * The initialization starts a new thread that connects to the given netdev
  * device and starts a link layer event loop.
