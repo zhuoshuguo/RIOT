495ab48e05a5f710eb42639b4d0c3611e96dd4a7
zhuoshuguo <shuguo.zhuo@inria.fr>
2017-04-17 17:41:39 +0200

    resume exp settings

--- examples/gnrc_networking/main.c
+++ examples/gnrc_networking/main.c
@@ -81,15 +81,11 @@ static void generate_and_send_pkt(void){
 	//expset[1] = gnrc_netdev2->lwmac.exp_duration;
 
 	/* exp total generate packet number */
-	expset[2] = 5;
+	expset[2] = 60;
 
 	/* the sequence of the command */
 	//expset[3] = 10 - send_counter;
 
-	char *add = "2001:db8::5844:2c50:d550:a312";
-    udp_send(add, port, expset, sizeof(expset), num, delay);
-
-#if 0
     if(send_counter == 0) {
     	char *add = "2001:db8::5844:55d:4a55:6f46"; //
         udp_send(add, port, expset, sizeof(expset), num, delay);
@@ -121,7 +117,6 @@ static void generate_and_send_pkt(void){
     	char *add = "2001:db8::5844:2b54:22bc:103e";//
         udp_send(add, port, expset, sizeof(expset), num, delay);
     }
-#endif
 }
 
 void *sender_thread(void *arg)
@@ -183,23 +178,23 @@ void *sender_thread(void *arg)
     char *instanceid = "1";
     _gnrc_rpl_dodag_root(instanceid, ipadd);
 
-	xtimer_sleep(100);
+	xtimer_sleep(150);
 
    exp_end = false;
 
    puts("generating exp start commands to nodes");
    while (1) {
-	   xtimer_sleep(5);
+	   xtimer_sleep(10);
 	   //xtimer_usleep((uint32_t) data_rate * 1000);
 
-	   if((send_counter < 1)&&(exp_end == false)){  //total_gene_num
+	   if((send_counter < 10)&&(exp_end == false)){  //total_gene_num
 		   for(int i=0; i<1; i++){
 			   generate_and_send_pkt();
 			   printf("send to %lu \n", send_counter);
 		   }
 		   send_counter ++;
 
-		   if(send_counter>=1) {
+		   if(send_counter>=10) {
 			   ;//printf("finished generate exp commands.\n");
 		   }
 	   }else {
