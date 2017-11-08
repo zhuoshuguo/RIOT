8f886f26492fb5fb21dbffa865dfda39967413da
zhuoshuguo <shuguo.zhuo@inria.fr>
2017-11-08 15:20:00 +0100

    add sender codes.

--- tests/gomach/main.c
+++ tests/gomach/main.c
@@ -28,19 +28,275 @@
 #include "thread.h"
 #include "shell.h"
 #include "shell_commands.h"
+#include "timex.h"
+#include <random.h>
+#include "net/gnrc.h"
+#include "net/gnrc/netif.h"
+#include "net/gnrc/netapi.h"
+#include "net/gnrc/netreg.h"
+#include "net/gnrc/nettype.h"
+#include "xtimer.h"
+#include <periph/rtt.h>
+
+/*
+#if FEATURE_PERIPH_RTC
+#include "periph/rtc.h"
+#endif
+*/
+
+#ifdef MODULE_LTC4150
+#include "ltc4150.h"
+#endif
 
 #ifdef MODULE_NETIF
 #include "net/gnrc/pktdump.h"
 #include "net/gnrc.h"
 #endif
 
+uint32_t send_counter;
+uint32_t send_counter1;
+uint32_t send_counter2;
+uint32_t own_address2;
+uint32_t exp_start_time;
+uint32_t exp_duration_ticks;
+
+static void generate_and_send_pkt(void){
+
+	    kernel_pid_t dev;
+	    uint8_t addr[8];
+	    size_t addr_len;
+	    gnrc_pktsnip_t *hdr;
+	    int16_t dev2;
+
+	    gnrc_pktsnip_t* pkt;
+	    uint32_t payload[20];
+
+	    send_counter++;
+
+	    payload[1] = own_address2;
+	    payload[4] = exp_start_time;
+	    payload[5] = rtt_get_counter();
+
+	    dev2 = 4;
+	    /* parse interface */
+	    dev = (kernel_pid_t)dev2;
+
+	    addr_len = 8;
+
+    	payload[3] = 0x0000331e;
+
+        payload[0] = send_counter;
+
+        //15:11:6b:10:65:fa:54:32
+
+        //15:11:6b:10:65:f6:4c:0a
+
+        //79:67:27:72:f4:57:9f:e6
+        addr[0] = 0x15;
+        addr[1] = 0x11;
+
+        addr[2] = 0x6b;
+        addr[3] = 0x10;
+
+        addr[4] = 0x65;
+        addr[5] = 0xf6;
+
+        addr[6] = 0x4c;
+        addr[7] = 0x0a;
+
+#if 0
+	    if(own_address2 == 0x79f6) {  //5ad6
+
+	    	//79:67:08:77:01:9f:33:1e
+
+	    		payload[3] = 0x0000331e;
+
+	            addr[0] = 0x79;
+	            addr[1] = 0x67;
+
+	            addr[2] = 0x08;
+	            addr[3] = 0x77;
+
+	            addr[4] = 0x01;
+	            addr[5] = 0x9f;
+
+	            addr[6] = 0x33;
+	            addr[7] = 0x1e;
+
+		        payload[0] = send_counter;
+		        //printf("%lx: %lu.\n", payload[3],send_counter);
+	    }else if(own_address2 == 0x4c66) {  //6f46
+			//79:67:08:77:01:9f:33:1e
+	    	payload[3] = 0x0000331e;
+
+            addr[0] = 0x79;
+            addr[1] = 0x67;
+
+            addr[2] = 0x08;
+            addr[3] = 0x77;
+
+            addr[4] = 0x01;
+            addr[5] = 0x9f;
+
+            addr[6] = 0x33;
+            addr[7] = 0x1e;
+
+		}else if(own_address2 == 0x383a) {  //1b1a
+			//79:67:08:77:01:9f:33:1e
+	    	payload[3] = 0x0000331e;
+
+	        addr[0] = 0x79;
+	        addr[1] = 0x67;
+
+	        addr[2] = 0x35;
+	        addr[3] = 0x7e;
+
+	        addr[4] = 0x54;
+	        addr[5] = 0x3a;
+
+	        addr[6] = 0x79;
+	        addr[7] = 0xf6;
+            //////////////////////////////////////////////////////////
+
+		}else if(own_address2 == 0x8032) {  //a312
+			//79:67:3c:7c:2b:2a:c1:3a
+	    	payload[3] = 0x0000331e;
+
+	        addr[0] = 0x79;
+	        addr[1] = 0x67;
+
+	        addr[2] = 0x35;
+	        addr[3] = 0x7e;
+
+	        addr[4] = 0x54;
+	        addr[5] = 0x3a;
+
+	        addr[6] = 0x79;
+	        addr[7] = 0xf6;
+
+		}else if(own_address2 == 0x2c02) {  //0f22
+			//79:67:0f:73:f6:73:80:32
+	    	payload[3] = 0x0000331e;
+
+            addr[0] = 0x79;
+            addr[1] = 0x67;
+
+            addr[2] = 0x0f;
+            addr[3] = 0x73;
+
+            addr[4] = 0xf6;
+            addr[5] = 0x73;
+
+            addr[6] = 0x80;
+            addr[7] = 0x32;
+
+		}
+
+#endif
+
+	    hdr = gnrc_netif_hdr_build(NULL, 0, addr, addr_len);
+	    if(hdr == NULL){
+	    	puts("app: netif buf null!");
+	    	return;
+	    }
+
+		/****** assemble and send the beacon ******/
+		pkt = gnrc_pktbuf_add(NULL, payload, sizeof(payload), GNRC_NETTYPE_UNDEF);
+		if(pkt == NULL) {
+
+			gnrc_pktbuf_release(hdr);
+
+			puts("app: data buf null!");
+		}else{
+		    LL_PREPEND(pkt, hdr);
+
+		    int res;
+		    res = gnrc_netapi_send(dev, pkt);
+
+		    if(res < 1){
+		    	puts("app: send data msg to mac failed!");
+		    }
+
+		    printf("p: %lx: %lu.\n", own_address2, send_counter);
+		}
+
+}
+
+
+void *sender_thread(void *arg)
+{
+    (void) arg;
+
+    uint32_t data_rate;
+
+    data_rate = 0;
+
+
+    send_counter = 0;
+    send_counter1 = 0;
+    send_counter2 = 0;
+
+    int16_t devpid;
+
+    devpid = 4;
+
+    uint8_t own_addr[2];
+
+    gnrc_netapi_get(devpid, NETOPT_ADDRESS, 0, &own_addr,
+                            sizeof(own_addr));
+
+
+    xtimer_sleep(10);
+
+    own_address2 = 0;
+    own_address2 = own_addr[0];
+    own_address2 = own_address2 << 8;
+    own_address2 |= own_addr[1];
+
+    printf("own add is %lx.\n", own_address2);
+
+	data_rate = 5;
+
+ 	uint32_t listen_period;
+   	listen_period = random_uint32_range(10, 30);
+ 	printf("random wait: %lu s.\n", listen_period);
+
+   	xtimer_sleep(listen_period);
+
+    while (1) {
+   	xtimer_sleep(data_rate);
+
+	for(int i=0; i<1; i++){
+		generate_and_send_pkt();
+	}
+   }
+
+    return NULL;
+}
+
+char second_thread_stack[THREAD_STACKSIZE_MAIN];
+
+
 int main(void)
 {
+#ifdef MODULE_LTC4150
+    ltc4150_start();
+#endif
+
+
 #ifdef MODULE_NETIF
-    gnrc_netreg_entry_t dump = GNRC_NETREG_ENTRY_INIT_PID(GNRC_NETREG_DEMUX_CTX_ALL,
-                                                          gnrc_pktdump_pid);
+    gnrc_netreg_entry_t dump;
+
+    dump.target.pid = gnrc_pktdump_pid;
+    dump.demux_ctx = GNRC_NETREG_DEMUX_CTX_ALL;
     gnrc_netreg_register(GNRC_NETTYPE_UNDEF, &dump);
 #endif
+
+//kernel_pid_t pid =
+    thread_create(second_thread_stack, sizeof(second_thread_stack),
+                            THREAD_PRIORITY_MAIN + 1, THREAD_CREATE_STACKTEST,
+                            sender_thread, NULL, "shuguo_app");
+
 
     (void) puts("Welcome to RIOT!");
 
