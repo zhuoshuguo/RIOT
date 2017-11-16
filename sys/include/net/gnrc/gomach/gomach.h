75ad1cff1ac3375eb3cc396e05ee695d754b1868
zhuoshuguo <shuguo.zhuo@inria.fr>
2017-11-16 16:54:47 +0100

    gomach: adjust for iotlab-m3 nodes.

--- sys/include/net/gnrc/gomach/gomach.h
+++ sys/include/net/gnrc/gomach/gomach.h
@@ -57,7 +57,7 @@ extern "C" {
  * receiver will not miss the preamble packet.
  */
 #ifndef GNRC_GOMACH_CP_DURATION_US
-#define GNRC_GOMACH_CP_DURATION_US        (100U * US_PER_MS)
+#define GNRC_GOMACH_CP_DURATION_US        (13U * US_PER_MS)
 #endif
 
 /**
