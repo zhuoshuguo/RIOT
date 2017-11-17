ef9953064c263885647ebd3c0a6e592fe3dcc534
zhuoshuguo <shuguo.zhuo@inria.fr>
2017-11-17 10:32:47 +0100

    tests/gomach: cleaned up used modules and Makefile.

--- tests/gomach/main.c
+++ tests/gomach/main.c
@@ -1,7 +1,5 @@
 /*
- * Copyright (C) 2008, 2009, 2010  Kaspar Schleiser <kaspar@schleiser.de>
- * Copyright (C) 2013 INRIA
- * Copyright (C) 2013 Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ * Copyright (C) 2017  INRIA
  *
  * This file is subject to the terms and conditions of the GNU Lesser
  * General Public License v2.1. See the file LICENSE in the top level
@@ -9,15 +7,13 @@
  */
 
 /**
- * @ingroup     examples
+ * @ingroup     tests
  * @{
  *
  * @file
- * @brief       Default application that shows a lot of functionality of RIOT
+ * @brief       Test application for testing the GoMacH implementation
  *
- * @author      Kaspar Schleiser <kaspar@schleiser.de>
- * @author      Oliver Hahm <oliver.hahm@inria.fr>
- * @author      Ludwig Knüpfer <ludwig.knuepfer@fu-berlin.de>
+ * @author      Shuguo Zhuo <shuguo.zhuo@inria.fr>
  *
  * @}
  */
@@ -28,21 +24,14 @@
 #include "thread.h"
 #include "shell.h"
 #include "shell_commands.h"
-
-#ifdef MODULE_NETIF
 #include "net/gnrc/pktdump.h"
 #include "net/gnrc.h"
-#endif
 
 int main(void)
 {
-#ifdef MODULE_NETIF
     gnrc_netreg_entry_t dump = GNRC_NETREG_ENTRY_INIT_PID(GNRC_NETREG_DEMUX_CTX_ALL,
                                                           gnrc_pktdump_pid);
     gnrc_netreg_register(GNRC_NETTYPE_UNDEF, &dump);
-#endif
-
-    (void) puts("Welcome to RIOT!");
 
     char line_buf[SHELL_DEFAULT_BUFSIZE];
     shell_run(NULL, line_buf, SHELL_DEFAULT_BUFSIZE);
