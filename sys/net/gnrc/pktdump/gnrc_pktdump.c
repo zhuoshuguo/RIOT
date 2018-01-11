7f2f025f22139d5653f2edb524d26f22c535561c
zhuoshuguo <shuguo.zhuo@inria.fr>
2018-01-11 16:31:31 +0100

    dump

--- sys/net/gnrc/pktdump/gnrc_pktdump.c
+++ sys/net/gnrc/pktdump/gnrc_pktdump.c
@@ -190,10 +190,8 @@ static void _dump(gnrc_pktsnip_t *pkt, uint32_t received_pkt_counter)
     	}
     }
 
-
-   // printf("s: %x, g: %lu, r: %lu, t: %lu. \n", addr[1], payload[0], reception_list[i], received_pkt_counter);
-
-   printf("%lx, %lu, %lu, %lu. \n", payload[1], payload[0], reception_list[i], received_pkt_counter);
+   //printf("%lx, %lu, %lu, %lu. \n", payload[1], payload[0], reception_list[i], received_pkt_counter);
+    printf("T:%lu. \n", received_pkt_counter);
 
    gnrc_pktbuf_release(pkt);
 }
