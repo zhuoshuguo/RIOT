8d2afc3280f72b1a2171d65473ca89c5b9f3519b
zhuo shuguo <shuguo.zhuo@inria.fr>
2018-01-31 12:43:30 +0800

    add final

--- sys/net/gnrc/link_layer/gomach/gomach.c
+++ sys/net/gnrc/link_layer/gomach/gomach.c
@@ -1883,7 +1883,7 @@ static void gomach_sleep(gnrc_netdev_t *gnrc_netdev)
 
 static void gomach_sleep_end(gnrc_netdev_t *gnrc_netdev)
 {
-    if ((RTT_TICKS_TO_MIN(rtt_get_counter()) >= 10) && (gnrc_netdev->gomach.exp_started == false)) {
+    if ((RTT_TICKS_TO_MIN(rtt_get_counter()) >= 15) && (gnrc_netdev->gomach.exp_started == false)) {
         gnrc_netdev->gomach.exp_started = true;
 
         /* Start duty cycle recording */
