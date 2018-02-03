2b83581927e80961f83d922e70d54e861c5ed154
zhuo shuguo <shuguo.zhuo@inria.fr>
2018-02-03 11:06:03 +0800

    use 60mint exp time

--- sys/net/gnrc/link_layer/gomach/gomach.c
+++ sys/net/gnrc/link_layer/gomach/gomach.c
@@ -1868,12 +1868,11 @@ static void gomach_sleep(gnrc_netdev_t *gnrc_netdev)
 static void gomach_sleep_end(gnrc_netdev_t *gnrc_netdev)
 {
 
-
-	if ((RTT_TICKS_TO_MIN(rtt_get_counter()) >= 15) && (gnrc_netdev->gomach.exp_started == false)) {
+	if ((RTT_TICKS_TO_MIN(rtt_get_counter()) >= 20) && (gnrc_netdev->gomach.exp_started == false)) {
 		gnrc_netdev->gomach.exp_started = true;
 	}
 
-	if ((RTT_TICKS_TO_MIN(rtt_get_counter()) >= 1500) && (gnrc_netdev->gomach.exp_end == false)) {
+	if ((RTT_TICKS_TO_MIN(rtt_get_counter()) >= 80) && (gnrc_netdev->gomach.exp_end == false)) {
 
 		gnrc_netdev->gomach.exp_end = true;
 		int dd;
