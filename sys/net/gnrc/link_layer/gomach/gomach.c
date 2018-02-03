07626401940e2c89538a065979da171f9a8a23cc
zhuo shuguo <shuguo.zhuo@inria.fr>
2018-02-03 13:36:10 +0800

    use formal

--- sys/net/gnrc/link_layer/gomach/gomach.c
+++ sys/net/gnrc/link_layer/gomach/gomach.c
@@ -1440,6 +1440,8 @@ static void gomach_send_RI_beacon(gnrc_netdev_t *gnrc_netdev)
 static void gomach_listen_init(gnrc_netdev_t *gnrc_netdev)
 {
     //puts("C");
+	printf("C:%d\n",RTT_TICKS_TO_MIN(rtt_get_counter()));
+
     /* Reset last_seq_info, for avoiding receiving duplicate packets.
      * To-do: remove this in the future? */
     for (int i = 0; i < GNRC_GOMACH_DUPCHK_BUFFER_SIZE; i++) {
@@ -1868,11 +1870,11 @@ static void gomach_sleep(gnrc_netdev_t *gnrc_netdev)
 static void gomach_sleep_end(gnrc_netdev_t *gnrc_netdev)
 {
 
-	if ((RTT_TICKS_TO_MIN(rtt_get_counter()) >= 20) && (gnrc_netdev->gomach.exp_started == false)) {
+	if ((RTT_TICKS_TO_MIN(rtt_get_counter()) >= 15) && (gnrc_netdev->gomach.exp_started == false)) {
 		gnrc_netdev->gomach.exp_started = true;
 	}
 
-	if ((RTT_TICKS_TO_MIN(rtt_get_counter()) >= 90) && (gnrc_netdev->gomach.exp_end == false)) {
+	if ((RTT_TICKS_TO_MIN(rtt_get_counter()) >= 60) && (gnrc_netdev->gomach.exp_end == false)) {
 
 		gnrc_netdev->gomach.exp_end = true;
 		int dd;
