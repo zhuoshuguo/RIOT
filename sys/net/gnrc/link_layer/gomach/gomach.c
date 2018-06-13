33472c137aa80e135064079ff694cb8368d2d69c
zhuo shuguo <shuguo.zhuo@inria.fr>
2018-06-13 04:02:43 +0200

    fix

--- sys/net/gnrc/link_layer/gomach/gomach.c
+++ sys/net/gnrc/link_layer/gomach/gomach.c
@@ -2154,8 +2154,8 @@ static void *_gnrc_gomach_thread(void *args)
                 duty = (gnrc_netdev->gomach.awake_duration_sum_ticks) * 100 /
                        (duty - gnrc_netdev->gomach.system_start_time_ticks);
 
-                printf("[GoMacH]: active-time (ms): %lu %% \n", (uint32_t)(gnrc_netdev->gomach.awake_duration_sum_ticks / 1000);
-                printf("[GoMacH]: life-time (ms): %lu %% \n", (uint32_t)((xtimer_now_usec64() - gnrc_netdev->gomach.system_start_time_ticks) / 1000);
+                printf("[GoMacH]: active-time (ms): %lu %% \n", (uint32_t)(gnrc_netdev->gomach.awake_duration_sum_ticks / 1000));
+                printf("[GoMacH]: life-time (ms): %lu %% \n", (uint32_t)((xtimer_now_usec64() - gnrc_netdev->gomach.system_start_time_ticks) / 1000));
                 printf("[GoMacH]: achieved radio duty-cycle: %lu %% \n", (uint32_t)duty);
                 break;
             }
