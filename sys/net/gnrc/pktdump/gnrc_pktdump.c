ba16a41632aae41d86f15fef62a5253e9fc0c767
zhuo shuguo <shuguo.zhuo@inria.fr>
2019-06-16 21:19:16 +0800

    add receive print

--- sys/net/gnrc/pktdump/gnrc_pktdump.c
+++ sys/net/gnrc/pktdump/gnrc_pktdump.c
@@ -215,7 +215,7 @@ static void _dump(gnrc_pktsnip_t *pkt, uint32_t received_pkt_counter)
     }
 #endif
 
-   // printf("s: %x, g: %lu, r: %lu, t: %lu. \n", addr[1], payload[0], reception_list[i], received_pkt_counter);
+    printf("s: %x, g: %lu, r: %lu, t: %lu. \n", addr[1], payload[0], reception_list[i], received_pkt_counter);
 
     //printf("%lx, %lu, %lu, %lu. \n", payload[1], payload[0], reception_list[i], received_pkt_counter);
 
