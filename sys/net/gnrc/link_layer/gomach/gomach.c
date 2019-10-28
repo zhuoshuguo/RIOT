16446032abc8375283945d212bd47992e59e7c29
zhuo shuguo <shuguo.zhuo@inria.fr>
2019-10-28 20:02:25 +0800

    set exptime to 60min

--- sys/net/gnrc/link_layer/gomach/gomach.c
+++ sys/net/gnrc/link_layer/gomach/gomach.c
@@ -1877,9 +1877,9 @@ static void gomach_sleep_end(gnrc_netdev_t *gnrc_netdev)
 		gnrc_netdev->gomach.exp_started = true;
 	}
 
-	printf("%ld\n",RTT_TICKS_TO_MIN(rtt_get_counter()));
+	//printf("%ld\n",RTT_TICKS_TO_MIN(rtt_get_counter()));
 
-	if ((RTT_TICKS_TO_MIN(rtt_get_counter()) >= 120) && (gnrc_netdev->gomach.exp_end == false)) {
+	if ((RTT_TICKS_TO_MIN(rtt_get_counter()) >= 60) && (gnrc_netdev->gomach.exp_end == false)) {
 
 		gnrc_netdev->gomach.exp_end = true;
 		int dd;
