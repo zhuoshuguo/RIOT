12ee9b3fc5a9adbd8a9dc2d2eeb85ba447c19724
zhuo shuguo <shuguo.zhuo@inria.fr>
2019-11-10 16:55:43 +0800

    power -2

--- sys/net/gnrc/link_layer/gomach/gomach.c
+++ sys/net/gnrc/link_layer/gomach/gomach.c
@@ -158,12 +158,14 @@ static void gomach_init(gnrc_netdev_t *gnrc_netdev)
     gnrc_netdev->tx.t2u_fail_count = 0;
 
 
+
     xtimer_sleep(5);
     int16_t u16_power;
-    u16_power = -7;
+    u16_power = -2;
     //printf("TXPower: %d dBm\n", u16_power);
     gnrc_netdev->dev->driver->set(gnrc_netdev->dev, NETOPT_TX_POWER, &u16_power, sizeof(u16_power));
     xtimer_sleep(1);
+
 
 
 
