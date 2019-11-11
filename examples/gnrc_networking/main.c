ad043883294e2c37607ce750767736cb1d497a69
Shuguo Zhuo <zhuosgzju@gmail.com>
2019-11-11 01:30:55 -0800

    test

--- examples/gnrc_networking/main.c
+++ examples/gnrc_networking/main.c
@@ -160,7 +160,7 @@ void *sender_thread(void *arg)
     while (1) {
         xtimer_sleep(10);
 
-        if (RTT_TICKS_TO_MIN(rtt_get_counter()) >= 20) {
+        if (RTT_TICKS_TO_MIN(rtt_get_counter()) >= 30) {
             break;
         }
     }
